ff0a409a49a4e5d10d37266390d7592d5915addc
EricChen <305044632@qq.com>
2024-04-01 21:58:19 -0700

    modify defalt index.html and make auth0 work

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,26 +1,37 @@
-import { Box,Flex, Text, Button, Image, useColorModeValue } from '@chakra-ui/react';
+import { Heading, Box,Flex, Text, Button, Image, useColorModeValue, Container, useBreakpointValue } from '@chakra-ui/react';
 import { FiUser } from 'react-icons/fi'; // Make sure to import the icon from 'react-icons/fi'
 
 export default function Header() {
 	const bg = useColorModeValue('blue.500', 'blue.800'); // Light mode: blue.500, dark mode: blue.800
 	const color = useColorModeValue('white', 'gray.200'); // Light mode: white text, dark mode: slightly darker text
+  const isMobile = useBreakpointValue({ base: true, md: false }); // If the screen is smaller than 48em, isMobile is true
 	return (
+    <Container maxW="1280px" w="100%" mx="auto" px={0}>
 		<Flex
       alignItems="center"
       justifyContent="space-between" 
       p="4"
       bg="blue.500"
       color="white"
+
     >
       <Box flex="1" ml="20px">
         <Image src="cat_logo.webp" alt="Logo" h="60px" w="auto" />
       </Box>
       <Box flex="2" textAlign="center"> 
-        <Text fontSize="xl" fontWeight="bold">CineFave</Text>
+      <Heading as="h1" size="xl" >
+    CineFave
+  </Heading>
+  <Text fontSize="md" mt={2} fontWeight="light" letterSpacing="wide">
+    Open movie review
+  </Text>
       </Box>
       <Box flex="1" textAlign="right" mr="20px">
-        <Button colorScheme="blue" leftIcon={<FiUser />}>Profile</Button>
+        <Button colorScheme="blue" h="60px" w="auto" leftIcon={<FiUser  size="1.5em" />}>
+          {isMobile ? null : "Profile"}
+        </Button>
       </Box>
     </Flex>
+    </Container>
 	);
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NotFound from "./components/NotFound";
 import Home from "./components/Home";
-import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 import { AuthTokenProvider } from "./AuthTokenContext";
+import AuthDebugger from "./components/AuthDebugger";
 
-import { ChakraProvider,Box  } from '@chakra-ui/react'
+import { ChakraProvider,Box, Container  } from '@chakra-ui/react'
 import Header from "./components/Header";
+import AppLayout from "./components/AppLayout";
 
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
 
 const requestedScopes = ["profile", "email"];
+
+function RequireAuth({ children }) {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  // If the user is not authenticated, redirect to the home page
+  if (!isLoading && !isAuthenticated) {
+    return <Navigate to="/" replace />;
+  }
+
+  // Otherwise, display the children (the protected page)
+  return children;
+}
+
 
 root.render(
   <React.StrictMode>
@@ -27,13 +42,24 @@ root.render(
     >
       <AuthTokenProvider>
       <ChakraProvider>
-        <BrowserRouter>
+        <BrowserRouter>        
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/verify-user" element={<AuthDebugger />} />
+            <Route
+              path="app"
+              element={
+                <RequireAuth>
+                  <AppLayout />
+                </RequireAuth>
+              }
+            ></Route>
             <Route path="*" element={<NotFound />} />
           </Routes>
+          
         </BrowserRouter>
+        
         </ChakraProvider>
       </AuthTokenProvider>
     </Auth0Provider>
